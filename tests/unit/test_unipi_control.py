c1ba75eafaa815215cddc2046a033797f8ce09c2
Michael Hacker <mh@superbox.one>
2023-12-02 07:07:02 +0100

    Fixed unipi_control.py tests

--- tests/unit/test_unipi_control.py
+++ tests/unit/test_unipi_control.py
@@ -2,13 +2,13 @@
 
 from argparse import Namespace
 
-from unipi_control.unipi_control import parse_args
+from unipi_control.unipi_control import UnipiControl
 
 
 class TestHappyPathUnipiControl:
     def test_parse_args(self) -> None:
         """Test cli arguments for 'unipi-control'."""
-        parser = parse_args(["-vv"])
+        parser = UnipiControl.parse_args(["-vv"])
 
         assert parser.verbose == 2
         assert isinstance(parser, Namespace)
